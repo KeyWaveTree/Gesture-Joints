5cb2f42f9448a07952acad2f786fd6c443e07cdb
KeyWaveTree <damrok0815@gmail.com>
2024-12-19 12:37:20 +0900

    add backup file

--- Ex_openCv/01.CameraVideo.py
+++ Ex_openCv/01.CameraVideo.py
@@ -1,12 +1,12 @@
 from cv2 import cv2
 
-cap = cv2.VideoCapture(1)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH,640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT,480)
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,800)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,800)
 
 while cv2.waitKey(30)<0:
     ret, frame=cap.read()
-    cv2.imshow("출력 예제", frame)
+    cv2.imshow("Video", frame)
 
 cap.release()
-cv2.destroyWindow()
+cv2.destroyAllWindows()
